9f6095dba2cc43eb3f374ae6e2db7140bc8521c0
Sunaina <skksunaina0192@gmail.com>
2025-09-02 18:57:48 +0530

    Fix Navcontext file import issue

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import App from './App.jsx'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom'
 import Stairs from './components/common/Stairs.jsx'
-import  NavContext  from './components/context/Navcontext.jsx'
+import  NavContext  from './components/context/NavContext.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
